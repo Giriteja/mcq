b05c1e8b8b8702e906dd7cac4a5a1d7cd6347b42
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-19 22:31:05 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -39,11 +39,42 @@ def generate_mcq(paragraph,url,headers):
 
 # Streamlit app layout
 st.title("MCQ Generator")
-paragraph = st.text_area("Enter a paragraph:", height=200)
-if st.button("Generate MCQs"):
-    if paragraph:
-        mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
-        st.write(mcqs)
-    else:
-        st.write("Please enter a paragraph to generate questions.")
+# Function to read the image and extract text
+def extract_text(image):
+    # Convert the image to a format suitable for OCR
+    opencv_image = np.array(image)  # Convert PIL image to OpenCV format
+    gray_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Tesseract OCR
+    extracted_text = pytesseract.image_to_string(gray_image)
+
+    return extracted_text
+
+# Upload image
+uploaded_image = st.file_uploader("Upload an image...", type=["png", "jpg", "jpeg"])
+
+# If an image is uploaded, display and process it
+if uploaded_image is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Extract text
+    text = extract_text(image)
+    paragraph = st.text_area("Enter a paragraph:",text, height=200)
+    if st.button("Generate MCQs"):
+       if paragraph:
+          mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
+          st.write(mcqs)
+       else:
+          st.write("Please enter a paragraph to generate questions.")
+          
+if uploaded_image is None:         
+	paragraph = st.text_area("Enter a paragraph:", height=200)
+	if st.button("Generate MCQs via text"):
+		if paragraph:
+			mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
+			st.write(mcqs)
+		else:
+			st.write("Please enter a paragraph to generate questions.")
 
