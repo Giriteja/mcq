b544c98c9932f7dce6bf96c7c9a16e090bd2d3c9
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-23 22:06:45 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -16,6 +16,8 @@ chatgpt_headers = {
     "content-type": "application/json",
     "Authorization":"Bearer {}".format(os.getenv("openaikey"))}
     
+tab1, tab2, tab3 = st.tabs(["MCQ", "Summary", "GIY"])
+    
 def generate_mcq(paragraph,url,headers):
     
     # Define the payload for the chat model
@@ -42,8 +44,33 @@ def generate_mcq(paragraph,url,headers):
     output = response_json['choices'][0]['message']['content']
     return output
 
+
+def generate_summary(paragraph,url,headers):
+    
+    # Define the payload for the chat model
+    messages = [
+        {"role": "system", "content": "Give Detail Summarization of the given content with headings"},
+        {"role": "user", "content": paragraph}
+    ]
+
+    chatgpt_payload = {
+        "model": "gpt-3.5-turbo-16k",
+        "messages": messages,
+        "temperature": 1.3,
+        "max_tokens": 10000,
+        "top_p": 1,
+        "stop": ["###"]
+    }
+
+    # Make the request to OpenAI's API
+    response = requests.post(url, json=chatgpt_payload, headers=headers)
+    response_json = response.json()
+
+    # Extract data from the API's response
+    #st.write(response_json)
+    output = response_json['choices'][0]['message']['content']
+    return output
 # Streamlit app layout
-st.title("MCQ Generator")
 # Function to read the image and extract text
 def extract_text(image):
     # Convert the image to a format suitable for OCR
@@ -55,31 +82,41 @@ def extract_text(image):
 
     return extracted_text
 
-# Upload image
-uploaded_image = st.file_uploader("Upload an image...", type=["png", "jpg", "jpeg"])
 
-# If an image is uploaded, display and process it
-if uploaded_image is not None:
-    # Display the uploaded image
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+with(tab1):
+	# Upload image
+	uploaded_image = st.file_uploader("Upload an image...", type=["png", "jpg", "jpeg"])
 
-    # Extract text
-    text = extract_text(image)
-    paragraph = st.text_area("Enter a paragraph:",text, height=200)
-    if st.button("Generate MCQs"):
-       if paragraph:
-          mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
-          st.write(mcqs)
-       else:
-          st.write("Please enter a paragraph to generate questions.")
-          
-if uploaded_image is None:         
-	paragraph = st.text_area("Enter a paragraph:", height=200)
-	if st.button("Generate MCQs via text"):
-		if paragraph:
-			mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
-			st.write(mcqs)
+	# If an image is uploaded, display and process it
+	if uploaded_image is not None:
+	    # Display the uploaded image
+		image = Image.open(uploaded_image)
+		st.image(image, caption="Uploaded Image", use_column_width=True)
+
+		# Extract text
+		text = extract_text(image)
+		paragraph = st.text_area("Enter a paragraph:",text, height=200)
+		if st.button("Generate MCQs"):
+			if paragraph:
+		  		mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
+		  		st.write(mcqs)
 		else:
 			st.write("Please enter a paragraph to generate questions.")
-
+		  
+	if uploaded_image is None:         
+		paragraph = st.text_area("Enter a paragraph:", height=200)
+		if st.button("Generate MCQs via text"):
+			if paragraph:
+				mcqs = generate_mcq(paragraph,chatgpt_url,chatgpt_headers)
+				st.write(mcqs)
+			else:
+				st.write("Please enter a paragraph to generate questions.")
+				
+with(tab2):
+	paragraph = st.text_area("Enter the text:", height=200)
+	if st.button("Generate Summary via text"):
+		if paragraph:
+			summ = generate_summary(paragraph,chatgpt_url,chatgpt_headers)
+			st.write(summ)
+		else:
+			st.write("Please enter the text to generate Summary.")
