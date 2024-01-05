a78fb8cc8b0e1ea635750ce1aa0fdf1322ab6edf
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-01-05 10:09:09 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -117,6 +117,7 @@ with(tab1):
 				
 with(tab2):
 	paragraph = st.text_area("Enter the text:", height=200)
+	
 	prompt = st.text_area("Enter the prompt:", height=200)
 	if st.button("Generate Summary via text"):
 		if paragraph:
