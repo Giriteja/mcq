9ebb51ff7837128a4b77fb7eeda0eda4a0e33b45
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-03-03 19:55:10 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -482,11 +482,11 @@ with(tab3):
 			st.write("Please enter the text to generate Summary.")
 
 with(tab4):
-	topic = st.text_area("Enter the topic for Assignment:", height=200)
-	prompt_topic = st.text_area("Enter the prompt:",key="topic", height=200)
+	topic_assign = st.text_area("Enter the topic for Assignment:", height=200)
+	prompt_topic_assign = st.text_area("Enter the prompt:",key="topic_assign", height=200)
 	if st.button("Generate Lesson Plan"):
 		if topic:
-			lp = generate_assignment(topic,chatgpt_url,chatgpt_headers,prompt_topic)
+			lp = generate_assignment(topic_assign,chatgpt_url,chatgpt_headers,prompt_topic_assign)
 			st.write(lp)
 			
 		else:
