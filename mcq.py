fda3609bc9cee432e00fd98129781da09ffde907
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-03-05 20:49:34 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -493,14 +493,20 @@ with(tab1):
 		  
 	if uploaded_image is None:         
 		paragraph = st.text_area("Enter a paragraph:", height=200)
+	    	class_name = subject_name = st.selectbox(
+	   			"Select class",
+	   	("10", "9","8","7","6"),key="subject"
+		subject_name = subject_name = st.selectbox(
+	   			"Select Subject",
+	   	("Physics", "Social","Biology","Chemistry"),key="subject"
 		#prompt = st.text_area("Enter the prompt:", height=200)
 		if st.button("Generate MCQs via text"):
 			if paragraph:
 				mcqs = run_conversation(paragraph)
 				st.write(json.loads(mcqs))
 				mcq_json=json.loads(mcqs)
-				#for j in mcq_json['questions']:
-		  			#st.write(j)
+				for j in mcq_json['questions']:
+		  			st.write(j)
 			else:
 				st.write("Please enter a paragraph to generate questions.")
 				
