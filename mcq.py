83ae19de5235f25ef8d8a462e9b8f13927577683
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-04-02 23:12:38 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -14,7 +14,27 @@ import pandas as pd
 import uuid
 #import pdfplumber
 import PyPDF2
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+data_cred={"type": "service_account",
+        "project_id": os.getenv("project_id"),
+        "private_key_id": os.getenv("private_key_id"),
+        "private_key": os.getenv("private_key").replace('\\n', '\n'),
+        "client_email": os.getenv("client_email"),
+        "client_id": os.getenv("client_id"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+	"universe_domain":"googleapis.com"}
+
+cred = credentials.Certificate(data_cred)
+ 
+app = firebase_admin.initialize_app(cred)
+ 
+db = firestore.client()
 
 client = OpenAI(
   api_key=os.getenv("openaikey"),  # this is also the default, it can be omitted
