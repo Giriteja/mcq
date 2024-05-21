432c93bbe21e998ee2575a85d6cfb5fa0c04cc7d
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-05-21 23:30:34 +0530

    Update mcq.py

--- mcq.py
+++ mcq.py
@@ -1261,7 +1261,7 @@ with(tab6):
                     json_struct_inter['front_image']=None
                     #st.write(json_struct)
                     cards.append(json_struct_inter)
-                json_struct['cards']=cards
+		json_struct['cards']=cards
 		json_struct['topic_id']=topic_id_mapping[topic_selected]
 		#st.write(json_struct)
 		brain_buster_query = db.collection('brain_busters').where('topic_id', '==', json_struct['topic_id']).stream()
